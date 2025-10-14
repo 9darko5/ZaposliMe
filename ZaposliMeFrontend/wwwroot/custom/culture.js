fa9da6f2801658056429a181c40e5b7714ed231a
darko95 <djuricdarko95@gmail.com>
2025-10-14 16:09:57 +0200

    ZME-16 small fixes

--- ZaposliMeFrontend/wwwroot/custom/culture.js
+++ ZaposliMeFrontend/wwwroot/custom/culture.js
@@ -1,4 +1,4 @@
-﻿export function getBrowserLanguage() {
+﻿function getBrowserLanguage() {
     return (navigator.languages && navigator.languages.length > 0)
         ? navigator.languages[0]
         : (navigator.language || "en");
